7eb1fc5ec87dc0f664702902e23c0c19e1ae1636
Josie Beaudoin <jtranbeau@gmail.com>
2019-11-04 16:16:59 -0500

    finished issue #4

---
+++ server/db/models/shoe.js
@@ -0,0 +1,27 @@
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Shoe = db.define('shoe', {
+  name: {
+    type: Sequelize.STRING
+  },
+  cateogory: {
+    type: Sequelize.INTEGER
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://tinyurl.com/y35fmc2q',
+    validate: {
+      isUrl: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  price: {
+    type: Sequelize.INTEGER
+  }
+})
+
+module.exports = Shoe
