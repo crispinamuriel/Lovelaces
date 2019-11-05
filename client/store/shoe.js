94ef9183e3fbcd9aa05c33d244c9f773c735210e
Sharmin Sultana <hi@sharmin.sultana>
2019-11-05 15:34:47 -0500

    redux: added reducer for shoes

---
+++ client/store/shoe.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+// ACTION TYPES
+const GOT_SHOES = 'GOT_SHOES'
+const GOT_CURRENT_SHOE = 'GOT_CURRENT_SHOE'
+
+// INITIAL STATE
+const initialState = {
+  all: [],
+  current: {}
+}
+
+// ACTION CREATORS
+const gotShoes = shoes => ({type: GOT_SHOES, shoes})
+const gotCurrentShoe = currentShoe => ({type: GOT_CURRENT_SHOE, currentShoe})
+
+// THUNK CREATORS
+export const getShoes = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/shoes')
+    dispatch(gotShoes(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const getCurrentShoe = shoeId => async dispatch => {
+  try {
+    const {data} = await axios.get(`/api/shoes/${shoeId}`)
+    dispatch(gotCurrentShoe(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// REDUCER
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GOT_SHOES:
+      return {...state, all: action.shoes}
+    case GOT_CURRENT_SHOE:
+      return {...state, current: action.currentShoe}
+    default:
+      return state
+  }
+}
