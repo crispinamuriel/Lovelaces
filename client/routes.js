a17d274da78af92bf2af265a9da97598740b4430
alewoo28 <alewoo28@gmail.com>
2019-11-05 18:01:14 -0500

    single product view

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, Shoe} from './components'
 import {me} from './store'
 
 /**
@@ -19,7 +19,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
+        <Route path="/shoes/:shoeId" component={Shoe} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
           <Switch>
