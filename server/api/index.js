f8e94ebfeca949f8df0cd8e782fec0ba3e1e3c1b
Josie Beaudoin <jtranbeau@gmail.com>
2019-11-06 17:38:09 -0500

    feat: able to log in with google

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/shoes', require('./shoes'))
 router.use('/orders', require('./orders'))
+router.use('/google', require('./oauth'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
