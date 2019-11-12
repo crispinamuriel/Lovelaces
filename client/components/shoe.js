89af91d8fe762997b3ab55bc91848eb6284a2e1d
Sharmin Sultana <hi@sharmin.sultana>
2019-11-12 16:43:16 -0500

    updated single shoe view with material-ui

--- client/components/shoe.js
+++ client/components/shoe.js
@@ -3,42 +3,56 @@ import {connect} from 'react-redux'
 import {getCurrentShoe} from '../store/shoe'
 import {addToUserCart} from '../store/order'
 import {
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
   Button,
   Typography,
-  IconButton,
-  TextField
+  TextField,
+  Grid,
+  Paper,
+  Toolbar,
+  Divider,
+  InputLabel,
+  Select,
+  MenuItem
 } from '@material-ui/core'
 
-import {
-  AddCircleOutlineRounded,
-  RemoveCircleOutlineRounded,
-  ShoppingCart
-} from '@material-ui/icons'
-
-const styles = {
+const style = {
   card: {
     maxWidth: 500
   },
   media: {
-    // objectFit: 'cover',
-    height: 250
-  }
+    objectFit: 'contain',
+    width: 325
+  },
+  paper: {
+    marginBottom: 20,
+    marginTop: 20,
+    maxWidth: 750,
+    minWidth: 600,
+    padding: 25
+  },
+  detailsContainer: {
+    marginTop: 20
+  },
+  shoeInfoPaper: {
+    padding: 20,
+    marginTop: 20,
+    marginBottom: 20,
+    maxWidth: 350,
+    minWidth: 350
+  },
+  input: {minWidth: 120, maxWidth: 120},
+  inputLabel: {fontSize: '.75rem'},
+  price: {marginTop: 10}
 }
 class Shoe extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      quantity: 0,
-      size: ''
+      quantity: 1,
+      size: 6
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.increase = this.increase.bind(this)
-    this.decrease = this.decrease.bind(this)
   }
 
   async handleSubmit(event) {
@@ -63,47 +77,13 @@ class Shoe extends Component {
     })
   }
 
-  increase() {
-    this.setState(prevState => ({
-      quantity: prevState.quantity + 1
-    }))
-  }
-
-  // increase() {
-  //   this.setState(prevState => {
-  //     if(prevState.quantity < this.props.current.inventory) {
-  //       return {
-  //         quantity: prevState.quantity + 1
-  //       }
-  //     } else {
-  //       return ;
-  //     }
-  //   });
-  // }
-
-  decrease() {
-    this.setState(prevState => ({
-      quantity: prevState.quantity - 1
-    }))
-  }
-
-  // decrease() {
-  //   this.setState(prevState => {
-  //     if(prevState.quantity > 0 ) {
-  //       return {
-  //         quantity: prevState.quantity + 1
-  //       }
-  //     } else {
-  //       return ;
-  //     }
-  //   });
-  // }
-
   componentDidMount() {
     this.props.getCurrentShoe(this.props.match.params.shoeId)
   }
 
   render() {
+    console.log(this.state)
+
     const categories = {
       1: 'Sneakers',
       2: 'Boots & Booties',
@@ -121,108 +101,182 @@ class Shoe extends Component {
       name
     } = this.props.current
 
-    const disabledIncrease = this.state.quantity === inventory
-    const disabledDecrease = this.state.quantity === 0
+    const max = inventory >= 10 ? 10 : inventory
 
     return (
-      <Card className="entire-shoe">
-        <CardMedia
-          className="shoe-media"
-          component="img"
-          img={imageUrl}
-          alt={`${name} image`}
-          title={`${name} image`}
-        />
-
-        <CardContent className="shoe-details">
-          <Typography component="h3" variant="h3">
-            {name}
-          </Typography>
-
-          <Typography variant="subtitle1" color="textSecondary">
-            Shoe type: {categories[category]}
-          </Typography>
-
-          <Typography variant="subtitle1" color="textSecondary">
-            Price:{'$' + (price / 100).toFixed(2)}{' '}
-          </Typography>
-
-          <Typography variant="subtitle1"> Product details: </Typography>
-
-          <Typography variant="body1" color="textSecondary">
-            {description}
-          </Typography>
-        </CardContent>
-
-        <div id="buy-container">
-          <form
-            className="purchase-area"
-            noValidate
-            autoComplete="off"
-            onSubmit={this.handleSubmit}
+      <Grid container justify="center">
+        <Paper style={style.paper}>
+          <Toolbar>
+            <Typography variant="h6">{name}</Typography>
+          </Toolbar>
+          <Divider variant="middle" />
+          <Grid
+            container
+            spacing={4}
+            justify="center"
+            style={style.detailsContainer}
           >
-            <TextField
-              select
-              name="size"
-              onChange={this.handleChange}
-              value={this.state.size}
-              helperText="Select size"
-            >
-              SIZES
-              <option value="noSize">select size</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </TextField>
+            <Grid item>
+              <img src={imageUrl} style={style.media} />
+            </Grid>
+            <Grid item>
+              <Paper style={style.shoeInfoPaper}>
+                <Typography variant="body2">{description}</Typography>
+                <Typography variant="body2" style={style.price}>
+                  ${(price / 100).toFixed(2)}
+                </Typography>
+                <form onSubmit={this.handleSubmit}>
+                  <div
+                    style={{display: 'flex', justifyContent: 'space-evenly'}}
+                  >
+                    <div>
+                      <InputLabel id="size" style={style.inputLabel}>
+                        Size
+                      </InputLabel>
+                      <Select
+                        labelId="size"
+                        value={this.state.size}
+                        onChange={this.handleChange}
+                        name="size"
+                        style={style.input}
+                      >
+                        <MenuItem value={6}>6</MenuItem>
+                        <MenuItem value={7}>7</MenuItem>
+                        <MenuItem value={8}>8</MenuItem>
+                        <MenuItem value={9}>9</MenuItem>
+                        <MenuItem value={10}>10</MenuItem>
+                      </Select>
+                    </div>
 
-            <TextField
-              select
-              name="quantity"
-              onChange={this.handleChange}
-              value={this.state.quantity}
-            >
-              <Typography variant="h5" gutterBottom>
-                {this.state.quantity}
-              </Typography>
-            </TextField>
+                    <div>
+                      <InputLabel id="quantity" style={style.inputLabel}>
+                        Quantity
+                      </InputLabel>
+                      <TextField
+                        labelId="quantity"
+                        value={this.state.quantity}
+                        onChange={this.handleChange}
+                        name="quantity"
+                        type="number"
+                        inputProps={{
+                          min: 1,
+                          max: max,
+                          step: 1
+                        }}
+                        style={style.input}
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <Button variant="contained" color="primary" type="submit">
+                      Add To Cart
+                    </Button>
+                  </div>
+                </form>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
 
-            <CardActions>
-              <IconButton
-                size="small"
-                color="primary"
-                disabled={disabledDecrease}
-                onClick={this.decrease}
-              >
-                <RemoveCircleOutlineRounded />
-              </IconButton>
+      // <Card className="entire-shoe">
+      //   <CardMedia
+      //     className="shoe-media"
+      //     component="img"
+      //     img={imageUrl}
+      //     alt={`${name} image`}
+      //     title={`${name} image`}
+      //   />
 
-              <Typography variant="body1" color="textSecondary">
-                Quantity: {this.state.quantity}
-              </Typography>
+      //   <CardContent className="shoe-details">
+      //     <Typography component="h3" variant="h3">
+      //       {name}
+      //     </Typography>
 
-              <IconButton
-                size="small"
-                color="primary"
-                disabled={disabledIncrease}
-                onClick={this.increase}
-              >
-                <AddCircleOutlineRounded />
-              </IconButton>
+      //     <Typography variant="subtitle1" color="textSecondary">
+      //       Shoe type: {categories[category]}
+      //     </Typography>
 
-              <Button
-                id="add-to-cart"
-                type="submit"
-                size="large"
-                startIcon={<ShoppingCart />}
-              >
-                Add to Cart
-              </Button>
-            </CardActions>
-          </form>
-        </div>
-      </Card>
+      //     <Typography variant="subtitle1" color="textSecondary">
+      //       Price:{'$' + (price / 100).toFixed(2)}{' '}
+      //     </Typography>
+
+      //     <Typography variant="subtitle1"> Product details: </Typography>
+
+      //     <Typography variant="body1" color="textSecondary">
+      //       {description}
+      //     </Typography>
+      //   </CardContent>
+
+      //   <div id="buy-container">
+      //     <form
+      //       className="purchase-area"
+      //       noValidate
+      //       autoComplete="off"
+      //       onSubmit={this.handleSubmit}
+      //     >
+      //       <TextField
+      //         select
+      //         name="size"
+      //         onChange={this.handleChange}
+      //         value={this.state.size}
+      //         helperText="Select size"
+      //       >
+      //         SIZES
+      //         <option value="noSize">select size</option>
+      //         <option value="6">6</option>
+      //         <option value="7">7</option>
+      //         <option value="8">8</option>
+      //         <option value="9">9</option>
+      //         <option value="10">10</option>
+      //       </TextField>
+
+      //       <TextField
+      //         select
+      //         name="quantity"
+      //         onChange={this.handleChange}
+      //         value={this.state.quantity}
+      //       >
+      //         <Typography variant="h5" gutterBottom>
+      //           {this.state.quantity}
+      //         </Typography>
+      //       </TextField>
+
+      //       <CardActions>
+      //         <IconButton
+      //           size="small"
+      //           color="primary"
+      //           disabled={disabledDecrease}
+      //           onClick={this.decrease}
+      //         >
+      //           <RemoveCircleOutlineRounded />
+      //         </IconButton>
+
+      //         <Typography variant="body1" color="textSecondary">
+      //           Quantity: {this.state.quantity}
+      //         </Typography>
+
+      //         <IconButton
+      //           size="small"
+      //           color="primary"
+      //           disabled={disabledIncrease}
+      //           onClick={this.increase}
+      //         >
+      //           <AddCircleOutlineRounded />
+      //         </IconButton>
+
+      //         <Button
+      //           id="add-to-cart"
+      //           type="submit"
+      //           size="large"
+      //           startIcon={<ShoppingCart />}
+      //         >
+      //           Add to Cart
+      //         </Button>
+      //       </CardActions>
+      //     </form>
+      //   </div>
+      // </Card>
     )
   }
 }
